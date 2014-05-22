e55140c281f4df2e9f16209bbcb13e50ccd14fb3
Dan Murphy <murphydbuffalo@gmail.com>
2014-05-22 17:22:16 -0400

    Began adding restrictions on accepted form inputs.

--- server.rb
+++ server.rb
@@ -2,11 +2,10 @@ require 'sinatra'
 require "CSV"
 require "pry"
 
-#display submitted articles
-#every article should have description, title, and url
-#url should link to relevant page inside new tab
+def already_submitted(url, previous_urls)
+  
+end
 
-#display for index
 get "/index" do 
   @articles = []
   CSV.foreach('articles.csv', headers: true, header_converters: :symbol) do |row|
@@ -15,28 +14,55 @@ get "/index" do
   erb :index	
 end
 
-#redirect host to index page
 get "/" do 
   redirect "/index"
 end
 
-#display for comments
 get "/comments" do 
   erb :comments	
 end
 
-#display for submit
 get "/submit" do
   erb :submit
 end
 
-#accepting sent data and creating a csv
 post "/submit" do
   title = params["title"]
   url = params["url"]
   description = params["description"]
+  # @short_desc_error = ensure_length(description)
   CSV.open("articles.csv", "a") do |file|
-    file << [title, url, description]
-  end
-  redirect "/index"
+      file << [title, url, description]
+    end
+    redirect "/index"
 end
+
+# def ensure_valid(url)
+#   if url 
+#     ok_to_save_submission? == false
+#     url_error = "Enter a valid URL, please : )"
+#   end 
+# end
+
+# def ok_to_save_submission?
+#   false
+# end
+
+# def ensure_no_blank(*forms)
+#   forms.any? {|input| input == nil} ? "No blank fields please :-)" : ok_to_save_submission? == true
+# end
+
+# def ensure_length(description)
+#   description.length < 20 ? "Enter a description of at least 20 characters, please :-)" : ok_to_save_submission? == true
+# end
+
+# @blank_form_error = ensure_no_blank(title, url, description)
+  # @short_desc_error = ensure_length(description)
+  # if ok_to_save_submission?
+  #   CSV.open("articles.csv", "a") do |file|
+  #     file << [title, url, description]
+  #   end
+  #   redirect "/index"
+  # else
+  #   redirect "/submit"
+  # end
